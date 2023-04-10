01ef3d645062a3d0dab370e9d443973c6ac54bdc
hyeji07 <zindex_ann@naver.com>
2023-04-10 19:24:14 +0900

    story book ing

--- src/components/Button/test/Button.test.tsx
+++ src/components/Button/test/Button.test.tsx
@@ -1,11 +1,28 @@
 import { RedBtn, BlueBtn, SmBtn, LgBtn } from '../stories/Button.stories';
 
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-const mainColor = 'brown';
-
-test('RedButton을 랜더링할 수 있다.', () => {
+/* test('RedBtn을 랜더링할 수 있다.', () => {
   render(<RedBtn {...RedBtn.args} />);
   expect(screen.getByRole('button')).toHaveTextContent(/Red/i);
   expect(screen.getByRole('button')).toHaveStyle('backgroundColor:red');
+});
+
+test('BlueBtn을 랜더링할 수 있다.', () => {
+  render(<BlueBtn {...BlueBtn.args} />);
+  expect(screen.getByRole('button')).toHaveTextContent(/Blue/i);
+  expect(screen.getByRole('button')).toHaveStyle('backgroundColor:');
+}); */
+
+test('SmBtn을 랜더링할 수 있다.', () => {
+  render(<SmBtn {...SmBtn.args} />);
+  expect(screen.getByRole('button')).toHaveTextContent(/Sm Btn/i);
+  expect(screen.getByRole('button')).toHaveStyle('backgroundColor:gray');
+});
+
+test('LgBtn을 랜더링할 수 있다.', () => {
+  render(<LgBtn {...LgBtn.args} />);
+  expect(screen.getByRole('button')).toHaveTextContent(/Lg Btn/i);
+  expect(screen.getByRole('button')).toHaveStyle('backgroundColor:gray');
 });
