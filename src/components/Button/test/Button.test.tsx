3d621dac0ed6b2e0ddea1734f86bc60ac5686220
hyeji07 <zindex_ann@naver.com>
2023-04-10 19:25:16 +0900

    test ing

--- src/components/Button/test/Button.test.tsx
+++ src/components/Button/test/Button.test.tsx
@@ -3,7 +3,7 @@ import { RedBtn, BlueBtn, SmBtn, LgBtn } from '../stories/Button.stories';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-/* test('RedBtn을 랜더링할 수 있다.', () => {
+test('RedBtn을 랜더링할 수 있다.', () => {
   render(<RedBtn {...RedBtn.args} />);
   expect(screen.getByRole('button')).toHaveTextContent(/Red/i);
   expect(screen.getByRole('button')).toHaveStyle('backgroundColor:red');
@@ -13,7 +13,7 @@ test('BlueBtn을 랜더링할 수 있다.', () => {
   render(<BlueBtn {...BlueBtn.args} />);
   expect(screen.getByRole('button')).toHaveTextContent(/Blue/i);
   expect(screen.getByRole('button')).toHaveStyle('backgroundColor:');
-}); */
+});
 
 test('SmBtn을 랜더링할 수 있다.', () => {
   render(<SmBtn {...SmBtn.args} />);
